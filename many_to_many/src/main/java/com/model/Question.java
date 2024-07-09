03dd4e332ec28bc2a267bcb35d82d27e10cf5776
Pavithrab15 <166628708+Pavithrab15@users.noreply.github.com>
2024-07-09 14:53:21 +0530

    Add files via upload

---
+++ many_to_many/src/main/java/com/model/Question.java
@@ -0,0 +1,45 @@
+package com.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="ques323")
+public class Question {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String qname;
+	
+	@ManyToMany(targetEntity= Answer.class,cascade=(CascadeType.ALL))
+	@JoinTable(name="q_ans323",joinColumns= { @JoinColumn(name = "q_id")},inverseJoinColumns= { @JoinColumn(name = "ans_id")})
+	
+	private List<Answer> answers;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getQname() {
+		return qname;
+	}
+	public void setQname(String qname) {
+		this.qname = qname;
+	}
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+}
---
+++ many_to_many/src/main/java/com/model/Test.java
@@ -0,0 +1,35 @@
+package com.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Transaction tr=null;
+		try(Session ses=HBUtil.getSf().openSession()) {
+			tr=ses.beginTransaction();
+			Answer a1=new Answer();
+			a1.setAnswername("one answer");
+			a1.setPostedBy("Ramesh");
+			Answer a2=new Answer();
+			a2.setAnswername("Second answer");
+			a2.setPostedBy("Sumesh");
+			List<Answer> li=new ArrayList<Answer>();
+			li.add(a1);
+			li.add(a2);
+			Question q1=new Question();
+			q1.setQname("First Question");
+			q1.setAnswers(li);
+			ses.merge(q1);
+			tr.commit();
+			ses.close();
+	}
+			catch(Exception e) {e.printStackTrace();}
+		}
+
+	}
